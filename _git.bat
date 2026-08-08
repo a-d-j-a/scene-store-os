d6c76f6cad9bc3a336d66a7d44b4bb32109cabe0
a-d-j-a <a-d-j-a@users.noreply.github.com>
2026-08-08 14:07:17 +0530

    fix build.sh: correct versions, musl-gcc toolchain, meson cross file

---
+++ _git.bat
@@ -0,0 +1,6 @@
+@echo off
+set "PATH=C:\Program Files\Git\cmd;C:\Program Files\GitHub CLI;C:\Program Files\Git\usr\bin;C:\Windows\System32\OpenSSH;%PATH%"
+cd /d "C:\Users\khalu\Desktop\iso"
+git add -A
+git commit -m "fix build.sh: correct versions, musl-gcc toolchain, meson cross file"
+git push origin master
