08eaa87b0523dac809c57b9538003aa9ee1a7966
a-d-j-a <a-d-j-a@users.noreply.github.com>
2026-08-10 16:37:09 +0530

    cmd cycle 7: rebuild rootfs+initramfs+iso (pick up rcS chmod fix)

---
+++ .cmd
@@ -0,0 +1,12 @@
+cd /workspaces/scene-store-os/iso
+sudo bash build.sh rootfs
+stat -c '%a %n' build/sysroot/etc/init.d/rcS build/sysroot/etc/init.d/scene-desktop
+sudo bash build.sh initramfs
+sudo bash build.sh iso
+pkill -f qemu-system; sleep 2
+rm -f /tmp/serial.log /tmp/scr4.png
+qemu-system-x86_64 -m 1024 -vga std -smp 2 -cdrom output/iso-custom-6.6.52.iso -boot d -display none -monitor unix:/tmp/qmon.sock,server,nowait -serial file:/tmp/serial.log -daemonize
+sleep 120
+echo "screendump /tmp/scr4.png" | socat - unix-connect:/tmp/qmon.sock
+cp /tmp/serial.log /tmp/scr4.png /workspaces/scene-store-os/
+echo CMD_CYCLE_7_DONE
