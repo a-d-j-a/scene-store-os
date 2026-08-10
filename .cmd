6192d1bfa92bf7a293fc6c446c6847e8e06133f3
a-d-j-a <kharzubair50@gmail.com>
2026-08-10 10:44:44 +0000

    cmd run

--- .cmd
+++
@@ -1,9 +0,0 @@
-cd /workspaces/scene-store-os
-echo "screendump /tmp/scrA.png" | socat - unix-connect:/tmp/qmon.sock
-sleep 10
-echo "screendump /tmp/scrB.png" | socat - unix-connect:/tmp/qmon.sock
-echo "sendkey ret" | socat - unix-connect:/tmp/qmon.sock
-sleep 3
-echo "screendump /tmp/scrC.png" | socat - unix-connect:/tmp/qmon.sock
-cp /tmp/scrA.png /tmp/scrB.png /tmp/scrC.png /workspaces/scene-store-os/
-echo CMD_CYCLE_4_DONE
