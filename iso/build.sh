31ef9008316abc6d124f604d907792c9bbb36690
songlinks <songlinks@dev.local>
2026-09-15 19:43:13 +0530

    fix: libxkbcommon tests=disabled

--- iso/build.sh
+++ iso/build.sh
@@ -640,6 +640,7 @@ build_libxkbcommon() {
         -Denable-x11=false -Denable-tools=false \
         -Denable-bash-completion=false \
         -Denable-xkbregistry=false \
+        -Dtests=disabled \
         || die "libxkbcommon meson setup failed"
     ninja -C _build -j"$JOBS" || die "libxkbcommon build failed"
     DESTDIR="$SYSROOT" ninja -C _build install || die "libxkbcommon install failed"
